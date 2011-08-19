fa42daea0603b4a6e793f53d1c8c13da6c0d56c5
tsharju <teemu.harju@gmail.com>
2011-08-19 10:20:22 +0300

    Setting dev version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'python-sendgrid',
-    version = '0.1.2',
+    version = '0.2.0dev',
     url = 'http://github.com/tsharju/python-sendgrid',
     license = 'BSD',
     description = 'Library for SendGrid REST API.',
