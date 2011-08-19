352b23cfba7bb675b5deae0e17b996bf1c6add41
tsharju <teemu.harju@gmail.com>
2011-08-19 10:27:40 +0300

    Implementations for part of the Newsletter APIs.

--- src/sendgrid/__init__.py
+++ src/sendgrid/__init__.py
@@ -15,7 +15,8 @@ class SendGridAPIError(Exception):
 class SendGridAPI(object):
     """API library to access the SendGrid REST API."""
 
-    BASE_URL = 'https://sendgrid.com/api/'
+    API_PROTOCOL = 'https'
+    API_DOMAIN = 'sendgrid.com'
     FORMAT = 'json'
 
     def __init__(self, api_user, api_key):
@@ -24,14 +25,15 @@ class SendGridAPI(object):
 
     def call(self, method, **kwargs):
         # newsletter api uses different url structure than other apis
-        if method.startswith('newsletter'):
+        if method.startswith('api_newsletter'):
             api_method = method.replace('_', '/')
         else:
             api_method = method.replace('_', '.')
 
-        url = '%s%s.%s' % (SendGridAPI.BASE_URL,
-                           api_method,
-                           SendGridAPI.FORMAT)
+        url = '%s://%s/%s.%s' % (SendGridAPI.API_PROTOCOL,
+                                 SendGridAPI.API_DOMAIN,
+                                 api_method,
+                                 SendGridAPI.FORMAT)
         kwargs.update({'api_user': self.api_user, 'api_key': self.api_key})
 
         result = json.loads(requests.get(url, params=kwargs).content)
@@ -39,6 +41,72 @@ class SendGridAPI(object):
         if 'error' in result:
             raise SendGridAPIError(result['error'])
         return result
+
+    def newsletter_add(self, identity, name, subject, text, html):
+        """Create a new Newsletter."""
+        self.api_newsletter_add(identity=identity, name=name,
+                                subject=subject, text=text, html=html)
+
+    def newsletter_edit(self, name, newname, identity, subject, text, html):
+        """Edit an existing Newsletter."""
+        self.api_newsletter_edit(name=name, newname=newname, identity=identity,
+                                 subject=subject, text=text, html=html)
+
+    def newsletter_get(self, name):
+        """Retrieve the contents of an existing Newsletter."""
+        return self.api_newsletter_get(name=name)
+
+    def newsletter_list(self, name=""):
+        """Retrieve a list of all existing Newsletters."""
+        return self.api_newsletter_list(name=name)
+
+    def newsletter_delete(self, name):
+        """Remove an existing Newsletter."""
+        return self.api_newsletter_delete(name=name)
+
+    def newsletter_lists_add(self, list, name=""):
+        """Create a new Recipient List."""
+        return self.api_newsletter_lists_add(list=list, name=name)
+
+    def newsletter_lists_edit(self, list, newlist):
+        """Rename a Recipient List."""
+        return self.api_newsletter_lists_edit(list=list, newlist=newlist)
+
+    def newsletter_lists_get(self, list=""):
+        """List all Recipient Lists on your account, or check if a particular
+        List exists."""
+        return self.api_newsletter_lists_get(list=list)
+
+    def newsletter_lists_delete(self, list):
+        """Remove a Recipient List from your account."""
+        return self.api_newsletter_lists_delete(list=list)
+
+    def newsletter_lists_email_add(self, list, data):
+        """Add one or more emails to a Recipient List."""
+        if type(data) == type(dict):
+            data = json.dumps(data)
+        return self.api_newsletter_lists_email_add(list=list, data=data)
+
+    def newsletter_lists_email_get(self, list, email=""):
+        """Get the email addresses and associated fields for a
+        Recipient list."""
+        return self.api_newsletter_lists_email_get(list, email)
+
+    def newsletter_lists_email_delete(self, list, email):
+        """Remove one or more emails from a Recipient List."""
+        return self.api_newsletter_lists_email_delete(list=list, email=email)
+
+    def newsletter_recipients_add(self, name, list):
+        """Add Recipient Lists to a Newsletter."""
+        return self.api_newsletter_recipients_add(name=name, list=list)
+
+    def newsletter_recipients_get(self, name):
+        """Retrieve the Recipient Lists attached to an existing Newsletter."""
+        return self.api_newsletter_recipients_get(name=name)
+
+    def newsletter_recipients_delete(self, name, list):
+        """Delete Recipient Lists from a Newsletter."""
+        return self.api_newsletter_recipients_delete(name=name, list=list)
 
     def __getattr__(self, method):
         def get(self, *args, **kwargs):
